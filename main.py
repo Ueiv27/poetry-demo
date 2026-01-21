2f4a0e60f151c172fbcc98997dd5773a8b2bc1ea
Andrea Savelli <vicisev22@gmail.com>
2026-01-21 09:48:30 +0100

    Fix Aggiunta main()

---
+++ main.py
@@ -0,0 +1,13 @@
+from src.poetry_demo.prova import sottrazione
+
+def main():
+    a = 10
+    b = 5
+    result = sottrazione(a, b)
+    print(f"La sottrazione di {a} e {b} è: {result}")
+
+
+if __name__ == "__main__":
+    main()
+
+    
